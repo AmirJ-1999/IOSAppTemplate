66a32ea09a20de39ed80553a7bd22d2d67ea8b58
Amir Jawad <112168029+AmirJ-1999@users.noreply.github.com>
2026-03-05 17:02:20 +0100

    Fixed toolbar issue

--- IOSAppTemplate/ContentView.swift
+++ IOSAppTemplate/ContentView.swift
@@ -80,7 +80,7 @@ struct ContentView: View {
             .padding()
             .navigationTitle("My Starter App")
             .toolbar {
-                ToolbarItem(placement: .topBarTrailing) {
+                ToolbarItem(placement: .navigationBarTrailing) {
                     Button("Reset") {
                         resetTasks()
                     }

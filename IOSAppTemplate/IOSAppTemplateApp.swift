8fb90430ec7b2c048921ca657f6c702c23df2885
Amir Jawad <amirjawad79@gmail.com>
2026-03-05 07:06:51 -0800

    Initial Commit

---
+++ IOSAppTemplate/IOSAppTemplateApp.swift
@@ -0,0 +1,17 @@
+//
+//  IOSAppTemplateApp.swift
+//  IOSAppTemplate
+//
+//  Created by Amir on 05/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct IOSAppTemplateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
